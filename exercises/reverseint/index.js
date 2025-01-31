8876afe044a77c750a79f22c4120a3d37809c5cd
duccini <guilhermeduccini@gmail.com>
2025-01-31 18:45:01 -0300

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,33 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reverse = n.toString().split("").reverse().join("");
+
+  return parseInt(reverse) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+/**
+ *  String(x) VS toString(x)
+ *  Number(x) VS parseInt(x)
+ */
+
+// GUI SOLUTION
+
+// function reverseInt(n) {
+//   const positive = n >= 0;
+
+//   const str = positive ? String(n) : String(n * -1);
+
+//   let reverse = "";
+
+//   for (char of str) {
+//     if (char) {
+//       reverse = char + reverse;
+//     }
+//   }
+
+//   return positive ? Number(reverse) : Number(reverse) * -1;
+// }
